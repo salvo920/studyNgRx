1c3072b93c261a3de8d3b2b16f045cec184be8bd
Salvatore Celano <salvatore.celano@ennova-research.com>
2024-04-29 17:36:01 +0200

    - feat module 8 NgRx in studyProject init store

--- 4_First_look_At_ngrx/APM-Demo0/src/app/products/state/product.action.ts
+++ 4_First_look_At_ngrx/APM-Demo0/src/app/products/state/product.action.ts
@@ -6,5 +6,5 @@ export const setCurrentProduct = createAction('[product] set current product', p
 export const clearCurrentProduct = createAction('[product] Clear current product');
 export const initCurrrentProduct = createAction('[product] init current product');
 export const loadProduct = createAction('[product] load product');
-export const loadProductSuccess = createAction('[product] load product success', props<{ products: Product[] }>);
-export const loadProductFail = createAction('[product] load product fail', props<{ error: String }>);
+export const loadProductSuccess = createAction('[product] load product success', props<{ products: Product[] }>());
+export const loadProductFail = createAction('[product] load product fail', props<{ error: String }>());
